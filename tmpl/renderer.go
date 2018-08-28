484cf386f33785f677d9067afea0bce914e02df9
Rodrigo Villablanca <villa061004@gmail.com>
2018-08-28 13:53:40 -0300

    fix

--- tmpl/renderer.go
+++ tmpl/renderer.go
@@ -6,7 +6,7 @@ import (
 
 //Renderer is responsible to write all html templates.
 type Renderer interface {
-	Render(string, interface{}, io.Writer)
+	Render(string, interface{}, io.Writer) error
 }
 
 // DefaultRenderer is the default Renderer implementation
@@ -15,9 +15,13 @@ type DefaultRenderer struct {
 }
 
 //Render execute the named template on the given writer
-func (dr *DefaultRenderer) Render(name string, data interface{}, w io.Writer) {
-	template := dr.provider.FindTemplate(name)
+func (dr *DefaultRenderer) Render(name string, data interface{}, w io.Writer) error {
+	template, err := dr.provider.FindTemplate(name)
+	if err != nil {
+		return err
+	}
 	template.Execute(w, data)
+	return nil
 }
 
 var (
