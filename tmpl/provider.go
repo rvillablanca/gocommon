b76aa3564b5c03f022b248d6afa521d49ad8ee19
Rodrigo Villablanca <villa061004@gmail.com>
2018-08-28 13:49:52 -0300

    no panicking

--- tmpl/provider.go
+++ tmpl/provider.go
@@ -7,8 +7,8 @@ import (
 
 //TemplateProvider is responsible to find a template for a given template name.
 type TemplateProvider interface {
-	FindTemplate(string) *template.Template
-	AddTemplate(string, ...string)
+	FindTemplate(string) (*template.Template, error)
+	AddTemplate(string, ...string) error
 }
 
 // DefaultTemplateProvider is the default template provider used
@@ -17,18 +17,23 @@ type DefaultTemplateProvider struct {
 }
 
 // FindTemplate finds a template by the given name
-func (t *DefaultTemplateProvider) FindTemplate(name string) *template.Template {
+func (t *DefaultTemplateProvider) FindTemplate(name string) (*template.Template, error) {
 	tmpl, ok := t.templates[name]
 	if !ok {
-		panic(fmt.Errorf("could not get template with name %v", name))
+		return nil, fmt.Errorf("could not get template with name %v", name)
 	}
-	return tmpl
+	return tmpl, nil
 }
 
 // AddTemplate creates a new template with the given name parsing all the files
-func (t *DefaultTemplateProvider) AddTemplate(name string, files ...string) {
-	tmpl := template.Must(template.ParseFiles(files...))
+func (t *DefaultTemplateProvider) AddTemplate(name string, files ...string) error {
+	tmpl, err := template.ParseFiles(files...)
+	if err != nil {
+		return err
+	}
+
 	t.templates[name] = tmpl
+	return nil
 }
 
 var _ TemplateProvider = &DefaultTemplateProvider{}
