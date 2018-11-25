dddae9d51fffab9bda072374c7af3fcc51c4f12d
Rodrigo Villablanca <villa061004@gmail.com>
2018-11-25 18:34:17 -0300

    session max age

--- ssn/ssn.go
+++ ssn/ssn.go
@@ -44,12 +44,14 @@ func InitCookieSession(maxAge int, name, path string) {
 	store = cookieSession
 }
 
-func InitRedisSession(size int, name, address, password, key string) error {
+func InitRedisSession(maxAgeStore int, name, address, password, key string) error {
+	sessionName = name
 	redisStore, err := redistore.NewRediStore(10, "tcp", address, password, []byte(key))
 	if err != nil {
 		return err
 	}
 
+	redisStore.SetMaxAge(maxAgeStore)
 	store = redisStore
 	return nil
 }
