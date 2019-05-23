012b5b7499edb46c599c0709a70b5fc019e5e2af
Rodrigo Villablanca <villa061004@gmail.com>
2019-05-23 19:03:55 -0400

    improvements: now some methods does not return errors

--- ruts/rut.go
+++ ruts/rut.go
@@ -1,86 +1,77 @@
 package ruts
 
 import (
-    "fmt"
-    "strconv"
-    "strings"
+	"fmt"
+	"strconv"
+	"strings"
 
-    "golang.org/x/text/language"
-    "golang.org/x/text/message"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
-func Clean(rut string) (string, error) {
-    if rut == "" || len(rut) < 1 || len(rut) > 12 {
-        return "", fmt.Errorf("invalid rut %s", rut)
-    }
-
-    rut = strings.Replace(rut, ".", "", -1)
-    rut = strings.Replace(rut, ",", "", -1)
-    rut = strings.Replace(rut, "-", "", -1)
-
-    return rut, nil
+func Clean(rut string) string {
+	rut = strings.Replace(rut, ".", "", -1)
+	rut = strings.Replace(rut, ",", "", -1)
+	rut = strings.Replace(rut, "-", "", -1)
+	return rut
 }
 
 func Parse(rut string) (int, error) {
-    rut, err := Clean(rut)
-    if err != nil {
-        return 0, err
-    }
+	if rut == "" || len(rut) < 1 || len(rut) > 12 {
+		return 0, fmt.Errorf("invalid rut %s", rut)
+	}
 
-    s := []rune(rut)
-    s = s[:len(s)-1]
-    r, err := strconv.Atoi(string(s))
-    if err != nil {
-        return 0, err
-    }
-    return r, nil
+	rut = Clean(rut)
+
+	s := []rune(rut)
+	s = s[:len(s)-1]
+	r, err := strconv.Atoi(string(s))
+	if err != nil {
+		return 0, err
+	}
+	return r, nil
 }
 
 func GetDV(rut int) rune {
-    t := rut
-    count := 0
-    index := 2
-    for ; t != 0; t /= 10 {
-        m := t % 10
-        count += m * index
-        index++
-        if index > 7 {
-            index = 2
-        }
-    }
-    r := count % 11
-    d := 11 - r
-    if d == 10 {
-        return 'K'
-    }
-    return rune(d + 48)
+	t := rut
+	count := 0
+	index := 2
+	for ; t != 0; t /= 10 {
+		m := t % 10
+		count += m * index
+		index++
+		if index > 7 {
+			index = 2
+		}
+	}
+	r := count % 11
+	d := 11 - r
+	if d == 10 {
+		return 'K'
+	}
+	return rune(d + 48)
 }
 
-func Validate(rut string) (bool, error) {
-    parsed, err := Parse(rut)
-    if err != nil {
-        return false, err
-    }
+func Validate(rut string) bool {
+	parsed, err := Parse(rut)
+	if err != nil {
+		return false
+	}
 
-    calculateDV := GetDV(parsed)
+	calculateDV := GetDV(parsed)
+	cleaned := Clean(rut)
+	cleanedRune := []rune(cleaned)
+	providedDV := cleanedRune[len(cleanedRune)-1]
 
-    cleaned, err := Clean(rut)
-    if err != nil {
-        return false, err
-    }
+	if providedDV == 'k' {
+		providedDV = 'K'
+	}
 
-    cleanedRune := []rune(cleaned)
-    providedDV := cleanedRune[len(cleanedRune ) - 1]
-
-    if providedDV == 'k' {
-        providedDV = 'K'
-    }
-
-    return calculateDV == providedDV, nil
+	return calculateDV == providedDV
 }
 
-func Format(rut int) string{
-    formatted := message.NewPrinter(language.Spanish).Sprintf("%d", rut)
-    dv := GetDV(rut)
-    return fmt.Sprintf("%s-%s", formatted, string(dv))
+func Format(rut int) string {
+	formatted := message.NewPrinter(language.Spanish).Sprintf("%d", rut)
+	dv := GetDV(rut)
+	return fmt.Sprintf("%s-%s", formatted, string(dv))
 }
--- ruts/rut_test.go
+++ ruts/rut_test.go
@@ -1,97 +1,85 @@
 package ruts
 
 import (
-    "testing"
+	"testing"
 
-    "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestDV(t *testing.T) {
-    assert.Equal(t, '3', GetDV(16089456))
-    assert.Equal(t, '3', GetDV(16416888))
-    assert.Equal(t, '9', GetDV(22652884))
-    assert.Equal(t, '9', GetDV(24322384))
-    assert.Equal(t, '2', GetDV(10858247))
-    assert.Equal(t, '6', GetDV(10462669))
-    assert.Equal(t, '9', GetDV(99999999))
-    assert.Equal(t, 'K', GetDV(12341246))
-    assert.Equal(t, 'K', GetDV(12341263))
+	assert.Equal(t, '3', GetDV(16089456))
+	assert.Equal(t, '3', GetDV(16416888))
+	assert.Equal(t, '9', GetDV(22652884))
+	assert.Equal(t, '9', GetDV(24322384))
+	assert.Equal(t, '2', GetDV(10858247))
+	assert.Equal(t, '6', GetDV(10462669))
+	assert.Equal(t, '9', GetDV(99999999))
+	assert.Equal(t, 'K', GetDV(12341246))
+	assert.Equal(t, 'K', GetDV(12341263))
 }
 
 func TestParse(t *testing.T) {
-    rut, err := Parse("16.089.456-3")
-    assert.Equal(t, 16089456, rut)
-    assert.Nil(t, err)
+	rut, err := Parse("16.089.456-3")
+	assert.Equal(t, 16089456, rut)
+	assert.Nil(t, err)
 
-    rut, err = Parse("160894563")
-    assert.Equal(t, 16089456, rut)
-    assert.Nil(t, err)
+	rut, err = Parse("160894563")
+	assert.Equal(t, 16089456, rut)
+	assert.Nil(t, err)
 
-    rut, err = Parse("19")
-    assert.Equal(t, 1, rut)
-    assert.Nil(t, err)
+	rut, err = Parse("19")
+	assert.Equal(t, 1, rut)
+	assert.Nil(t, err)
 
-    rut, err = Parse("99.999.999-9")
-    assert.Equal(t, 99999999, rut)
-    assert.Nil(t, err)
+	rut, err = Parse("99.999.999-9")
+	assert.Equal(t, 99999999, rut)
+	assert.Nil(t, err)
 
-    rut, err = Parse("")
-    assert.Equal(t, 0, rut)
-    assert.NotNil(t, err)
+	rut, err = Parse("")
+	assert.Equal(t, 0, rut)
+	assert.NotNil(t, err)
 }
 
 func TestValidate(t *testing.T) {
-    valid, err := Validate("16.089.456-3")
-    assert.Nil(t, err)
-    assert.True(t, valid)
+	valid := Validate("16.089.456-3")
+	assert.True(t, valid)
 
-    valid, err = Validate("16.416.888-3")
-    assert.Nil(t, err)
-    assert.True(t, valid)
+	valid = Validate("16.416.888-3")
+	assert.True(t, valid)
 
-    valid, err = Validate("226528849")
-    assert.Nil(t, err)
-    assert.True(t, valid)
+	valid = Validate("226528849")
+	assert.True(t, valid)
 
-    valid, err = Validate("243223849")
-    assert.Nil(t, err)
-    assert.True(t, valid)
+	valid = Validate("243223849")
+	assert.True(t, valid)
 
-    valid, err = Validate("12341246k")
-    assert.Nil(t, err)
-    assert.True(t, valid)
+	valid = Validate("12341246k")
+	assert.True(t, valid)
 
-    valid, err = Validate("12341246K")
-    assert.Nil(t, err)
-    assert.True(t, valid)
+	valid = Validate("12341246K")
+	assert.True(t, valid)
 
-    valid, err = Validate("11.111.111-1")
-    assert.Nil(t, err)
-    assert.True(t, valid)
+	valid = Validate("11.111.111-1")
+	assert.True(t, valid)
 
-    valid, err = Validate("104626691")
-    assert.Nil(t, err)
-    assert.False(t, valid)
+	valid = Validate("104626691")
+	assert.False(t, valid)
 
-    valid, err = Validate("11.111.111-k")
-    assert.Nil(t, err)
-    assert.False(t, valid)
+	valid = Validate("11.111.111-k")
+	assert.False(t, valid)
 
-    valid, err = Validate("")
-    assert.NotNil(t, err)
-    assert.False(t, valid)
+	valid = Validate("")
+	assert.False(t, valid)
 
-    valid, err = Validate("19")
-    assert.Nil(t, err)
-    assert.True(t, valid)
+	valid = Validate("19")
+	assert.True(t, valid)
 }
 
 func TestFormat(t *testing.T) {
-    assert.Equal(t, "16.089.456-3", Format(16089456))
-    assert.Equal(t, "16.416.888-3", Format(16416888))
-    assert.Equal(t, "24.322.384-9", Format(24322384))
-    assert.Equal(t, "22.652.884-9", Format(22652884))
-    assert.Equal(t, "11.111.111-1", Format(11111111))
-    assert.Equal(t, "1-9", Format(1))
+	assert.Equal(t, "16.089.456-3", Format(16089456))
+	assert.Equal(t, "16.416.888-3", Format(16416888))
+	assert.Equal(t, "24.322.384-9", Format(24322384))
+	assert.Equal(t, "22.652.884-9", Format(22652884))
+	assert.Equal(t, "11.111.111-1", Format(11111111))
+	assert.Equal(t, "1-9", Format(1))
 }
-
