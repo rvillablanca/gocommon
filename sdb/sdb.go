40a38004b8b57f7091954262331c4dd49bce02a3
Rodrigo Villablanca <villa061004@gmail.com>
2019-09-08 17:34:23 -0300

    refactor

--- sdb/sdb.go
+++ sdb/sdb.go
@@ -3,44 +3,44 @@ package sdb
 import "database/sql"
 
 type ServiceDB struct {
-	db *sql.DB
-	tx *sql.Tx
+	DB *sql.DB
+	TX *sql.Tx
 }
 
 func (sdb *ServiceDB) Exec(query string, args ...interface{}) (sql.Result, error) {
-	if sdb.db != nil {
-		return sdb.db.Exec(query, args...)
+	if sdb.DB != nil {
+		return sdb.DB.Exec(query, args...)
 	}
 
-	return sdb.tx.Exec(query, args...)
+	return sdb.TX.Exec(query, args...)
 }
 
 func (sdb *ServiceDB) QueryRow(query string, args ...interface{}) *sql.Row {
-	if sdb.db != nil {
-		return sdb.db.QueryRow(query, args...)
+	if sdb.DB != nil {
+		return sdb.DB.QueryRow(query, args...)
 	}
 
-	return sdb.tx.QueryRow(query, args...)
+	return sdb.TX.QueryRow(query, args...)
 }
 
 func (sdb *ServiceDB) Query(query string, args ...interface{}) (*sql.Rows, error) {
-	if sdb.db != nil {
-		return sdb.db.Query(query, args...)
+	if sdb.DB != nil {
+		return sdb.DB.Query(query, args...)
 	}
 
-	return sdb.tx.Query(query, args...)
+	return sdb.TX.Query(query, args...)
 }
 
 func NewFromTx(tx *sql.Tx) *ServiceDB {
 	return &ServiceDB{
-		db: nil,
-		tx: tx,
+		DB: nil,
+		TX: tx,
 	}
 }
 
 func NewFromDB(db *sql.DB) *ServiceDB {
 	return &ServiceDB{
-		db: db,
-		tx: nil,
+		DB: db,
+		TX: nil,
 	}
 }
